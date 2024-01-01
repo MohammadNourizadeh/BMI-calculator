e8cf1ed9f961af466b6fc91dda6adff15e6b790c
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-01-01 20:20:10 +0330

    working on js and bmi parameter added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,44 @@
 const weightInput = document.getElementById('weight-input');
 const heightInput = document.getElementById('height-input');
-const height = document.getElementById('height');
-const weigh = document.getElementById('weigh');
+const btn = document.getElementById('btn');
+const resultDiv = document.getElementById('result-div');
 
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const res = weightInput.value / (heightInput.value ** 2);
+
+    if (res < 18.5) {
+        resultDiv.innerHTML = `
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <img src="./assets/img/less18.5.png" alt="underweight">
+            <span class="text-uppercase">underweight</span>
+        </div>
+        `
+    } 
+    else if (18.5 <= res <= 24.9) {
+        resultDiv.innerHTML = `
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <img src="./assets/img/betw18.5-24.9.png" alt="underweight">
+            <span class="text-uppercase">normal</span>
+        </div>
+        `
+    } 
+    else if (25 <= res <= 29.9) {
+        resultDiv.innerHTML = `
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <img src="./assets/img/betw25.0-29.9.png" alt="underweight">
+            <span class="text-uppercase">overweight</span>
+        </div>
+        `
+    } 
+    else if (30 <= res <= 34.9) {
+        resultDiv.innerHTML = `
+        <div class="d-flex justify-content-center align-items-center flex-column">
+            <img src="./assets/img/betw30.0-34.9.png" alt="underweight">
+            <span class="text-uppercase">obese</span>
+        </div>
+        `
+    }
+})
 
