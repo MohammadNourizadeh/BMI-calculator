b3da81df4dd7b1ca96703de30256d0cc67dad999
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-01-01 18:54:21 +0330

    layout of project changed

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const weightInput = document.getElementById('weight-input');
+const heightInput = document.getElementById('height-input');
+const height = document.getElementById('height');
+const weigh = document.getElementById('weigh');
+
+
