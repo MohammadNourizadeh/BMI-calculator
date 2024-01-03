598f18bb60495674dd2aeb642f35fbd859e02b1f
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-01-03 11:22:21 +0330

    js of project done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,39 +6,54 @@ const resultDiv = document.getElementById('result-div');
 btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const res = weightInput.value / (heightInput.value ** 2);
+    if (weightInput.value === '') {
+        weightInput.classList.add('border-2', 'border-danger')
+    } else if (weightInput.value !== '') {
+        weightInput.classList.remove('border-2', 'border-danger')
+    };
 
-    if (res < 18.5) {
-        resultDiv.innerHTML = `
+    if (heightInput.value === '') {
+        heightInput.classList.add('border-2', 'border-danger')
+    } else if (heightInput.value !== '') {
+        heightInput.classList.remove('border-2', 'border-danger')
+    };
+
+    if (heightInput.value !== '' && weightInput.value !== '') {
+        const res = weightInput.value / ((heightInput.value * .01) ** 2);
+
+        if (res < 18.5) {
+            resultDiv.innerHTML = `
         <div class="d-flex justify-content-center align-items-center flex-column">
             <img src="./assets/img/less18.5.png" alt="underweight">
             <span class="text-uppercase">underweight</span>
         </div>
         `
-    } 
-    else if (18.5 <= res <= 24.9) {
-        resultDiv.innerHTML = `
+        }
+        if (18.5 <= res && res <= 24.9) {
+            resultDiv.innerHTML = `
         <div class="d-flex justify-content-center align-items-center flex-column">
             <img src="./assets/img/betw18.5-24.9.png" alt="underweight">
             <span class="text-uppercase">normal</span>
         </div>
         `
-    } 
-    else if (25 <= res <= 29.9) {
-        resultDiv.innerHTML = `
+        }
+        if (25 <= res && res <= 29.9) {
+            resultDiv.innerHTML = `
         <div class="d-flex justify-content-center align-items-center flex-column">
             <img src="./assets/img/betw25.0-29.9.png" alt="underweight">
             <span class="text-uppercase">overweight</span>
         </div>
         `
-    } 
-    else if (30 <= res <= 34.9) {
-        resultDiv.innerHTML = `
+        }
+        if (30 <= res) {
+            resultDiv.innerHTML = `
         <div class="d-flex justify-content-center align-items-center flex-column">
             <img src="./assets/img/betw30.0-34.9.png" alt="underweight">
             <span class="text-uppercase">obese</span>
         </div>
         `
-    }
-})
+        }
+
+    };
+});
 
